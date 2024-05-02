0ad44db41d8f8d72d7d96c8f8822f17541ac7643
Taegyu <mordol@gmail.com>
2024-05-02 12:24:08 +0900

    - Added shader and zombie prefab for SRP - Added a test scene and grid spawner

---
+++ Assets/Scripts/GridSpawner.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridSpawner : MonoBehaviour
+{
+    public GameObject spawnPrefab;
+    public int spawnCount = 1000;
+    public float gridSize = 0.5f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (spawnPrefab == null || spawnCount <= 0)
+        {
+            return;
+        }
+
+        var gridCount = Mathf.CeilToInt(Mathf.Sqrt(spawnCount));
+        var halfGridCount = gridCount / 2;
+        
+        for (int x = -halfGridCount; x < halfGridCount; x++)
+        {
+            for (int z = -halfGridCount; z < halfGridCount; z++)
+            {
+                Vector3 spawnPosition = new Vector3(
+                    x * gridSize,
+                    0,
+                    z * gridSize
+                );
+                
+                Instantiate(spawnPrefab, spawnPosition, Quaternion.identity);
+            }
+        }
+    }
+}
